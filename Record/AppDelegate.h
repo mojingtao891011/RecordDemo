0861e7bb91f664d114ab2f85eee26e47c3316582
莫景涛 <13677785116@163.com>
2014-03-25 09:26:40 +0800

    Initial Commit

---
+++ Record/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Record
+//
+//  Created by 莫景涛 on 14-3-25.
+//  Copyright (c) 2014年 莫景涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Record/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Record
+//
+//  Created by 莫景涛 on 14-3-25.
+//  Copyright (c) 2014年 莫景涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
